7705ed4524968014a48c9e36a1be46a4da5a78da
+N <1554735+nurih@users.noreply.github.com>
2021-04-26 22:46:45 -0700

    Update spy-adapted.js

--- spy-adapted.js
+++ spy-adapted.js
@@ -2,14 +2,14 @@
 // Programming Quantum Computers
 //   by Eric Johnston, Nic Harrigan and Mercedes Gimeno-Segovia
 //   O'Reilly Media
-// To run this online, go to http://oreilly-qc.github.io?p=2-4
+// To run this onlinQ, go to http://oreilly-qc.github.io?p=2-4
 
 qc.reset(3);
 qc.discard();
 
-var alf = qint.new(1, 'alf');
-var line = qint.new(1, 'line');
-var cat = qint.new(1, 'cat');
+var gibQ = qint.new(1, 'gibQ');
+var linQ = qint.new(1, 'linQ');
+var bexQ = qint.new(1, 'bexQ');
 
 function getRandomBit(qubit) {
     qubit.write(0);
@@ -22,22 +22,22 @@ function dump() {
 }
 // Generate two random bits
 qc.label('2 random bits');
-var originalHad = getRandomBit(alf);
-var originalValue = getRandomBit(alf);
+var originalHad = getRandomBit(gibQ);
+var originalValue = getRandomBit(gibQ);
 
-// Prepare alf qubit
-alf.write(0);
+// Prepare gibQ qubit
+gibQ.write(0);
 qc.label('set value');
 if (originalValue)
-    alf.not();
+    gibQ.not();
 
 qc.label('apply had?');
 if (originalHad)
-    alf.had();
+    gibQ.had();
 
 
 // Send the qubit!
-line.exchange(alf);
+linQ.exchange(gibQ);
 
 // Activate the spy
 var isSpyPresent = true;
@@ -45,28 +45,26 @@ if (isSpyPresent) {
     var spy_had = 1;
     qc.label('spy');
     if (spy_had)
-        line.had();
-    stolen_data = line.read();
-    line.write(0);
+        linQ.had();
+    stolen_data = linQ.read();
+    linQ.write(0);
     if (stolen_data)
-        line.not();
+        linQ.not();
     if (spy_had)
-        line.had();
+        linQ.had();
 }
 
 // Receive the qubit!
-var receivedHad = getRandomBit(cat);
-line.exchange(cat);
+var receivedHad = getRandomBit(bexQ);
+linQ.exchange(bexQ);
 qc.label('apply had');
 if (receivedHad)
-    cat.had();
+    bexQ.had();
 qc.label('read value');
-receivedValue = cat.read();
+receivedValue = bexQ.read();
 
-// Now Alice emails Bob to tell
-// him her had setting and value.
-// If the had setting matches and the
-// value does not, there's a spy!
+// Gib supplies Bex with original Had and Value
+// If the had setting matches and the value does not, there's a spy!
 dump();
 if (originalHad == receivedHad) {
     if (originalValue != receivedValue) {
@@ -76,5 +74,5 @@ if (originalHad == receivedHad) {
         qc.print('Spy not detected\n');
     }
 } else {
-    qc.print('Unexpected mismatch\n');
+    qc.print('Unclear\n');
 }
